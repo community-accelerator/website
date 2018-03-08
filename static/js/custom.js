fcb5bd00923d90c106c2eb520c0d2c183b7ae311
Jorge <jbergasa@yahoo.co.uk>
2018-03-08 13:22:51 +0000

    Changed form submittion email

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,5 +1,6 @@
 $("#contactForm").submit(function() {
-    var addr = String.fromCharCode(106,98,101,114,103,97,115,97,64,121,97,104,111,111,46,99,111,46,117,107);
+    var addr = String.fromCharCode(99, 111, 109, 109, 117, 110, 105, 116, 121, 45, 97, 99, 99, 101, 108, 101, 114, 97, 116, 111, 114, 64, 107, 105, 116, 105, 99, 111, 119, 46, 99, 111, 109);
     if (!$(this).attr('action').endsWith(addr))
         $(this).attr('action', $(this).attr('action') + addr);
 })
+
